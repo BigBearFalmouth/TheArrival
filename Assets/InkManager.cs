3829482e3fec4316c06feea2695a1a9852ad7479
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2018-11-14 23:03:57 +0000

    Added in basic inkle integration and test scene

---
+++ Assets/InkManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+using UnityEngine.UI;
+using System;
+
+public class InkManager : MonoBehaviour {
+
+    public TextAsset inkAsset;
+    public Button buttonPrefab;
+    public Text textPrefab;
+    public Canvas canvas;
+
+    Story inkStory;
+
+    private void Awake()
+    {
+        inkStory = new Story(inkAsset.text);
+    }
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    while (inkStory.canContinue)
+        {
+            Text storyText = Instantiate<Text>(textPrefab);
+            storyText.text = inkStory.Continue();
+
+            storyText.transform.SetParent(canvas.transform, false);
+            if (inkStory.currentChoices.Count > 0)
+            {
+                for (int i = 0; i < inkStory.currentChoices.Count; i++)
+                {
+                    Choice choice = inkStory.currentChoices[i];
+                    Button button = Instantiate<Button>(buttonPrefab);
+                    button.GetComponentInChildren<Text>().text = choice.text;
+                    button.onClick.AddListener(() => OnClickButton(choice));
+
+                    button.transform.SetParent(canvas.transform, false);
+                }
+            }
+        }
+
+	}
+
+    private void OnClickButton(Choice choice)
+    {
+        inkStory.ChooseChoiceIndex(choice.index);
+
+        foreach(Transform child in canvas.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+}
