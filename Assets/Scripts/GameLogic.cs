500f9798026bb61537a6e2354d1374cc908b4d34
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2018-11-26 22:56:18 +0000

    Added the layout for the game on PC

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+
+    public Animator InventoryAnimator;
+    public bool ShowInventory = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void ShowInventoryClicked()
+    {
+        ShowInventory = !ShowInventory;
+        InventoryAnimator.SetBool("ShowPanel", ShowInventory);
+    }
+}
